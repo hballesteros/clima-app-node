9aa5e8f2b28678c7a7e206931e80247a1018c004
Hugo <hugo@boahstudio.com>
2023-03-25 08:03:22 -0300

    Primer Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require('colors')
+const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer')
+const Busquedas = require('./models/busquedas')
+
+
+const main = async() => {
+
+    const busquedas = new Busquedas
+    let opt
+
+    do {
+        opt = await inquirerMenu()
+
+        switch (opt) {
+            case 1: // Buscar ciudad
+                // Mostrar mensaje
+                const lugar = await leerInput('Ciudad: ')
+                console.log(lugar);
+
+                // Buscar los lugares
+
+                // Seleccionar el lugar
+
+                // Clima
+
+                // Mostrar resultados
+                console.log('\nInformación de la ciudad\n'.green)
+                console.log('Ciudad:', )
+                console.log('Lat:', )
+                console.log('Lng:', )
+                console.log('Temperatura:', )
+                console.log('Mínima:', )
+                console.log('Máxima:', )
+
+                break
+            
+            case 2: // Historial
+                console.log(opt);
+                break
+        }
+
+        await pausa()
+
+    } while ( opt !== 0 )
+
+}
+
+main()
